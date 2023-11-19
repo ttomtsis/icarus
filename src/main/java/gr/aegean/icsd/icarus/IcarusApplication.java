3e94c285971592469d09335411b7351204a3814f
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-11-19 11:35:28 +0200

    First commit

---
+++ src/main/java/gr/aegean/icsd/icarus/IcarusApplication.java
@@ -0,0 +1,13 @@
+package gr.aegean.icsd.icarus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcarusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcarusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/aegean/icsd/icarus/IcarusApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aegean.icsd.icarus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcarusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
