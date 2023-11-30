620c0c078e5f33606473fe2a40a6e1717655f5ed
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-11-30 20:56:23 +0200

    Added custom exceptions for a variety of cases

---
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/ProviderAccountNotFoundException.java
@@ -0,0 +1,9 @@
+package gr.aegean.icsd.icarus.util.exceptions;
+
+public class ProviderAccountNotFoundException extends RuntimeException {
+
+    public ProviderAccountNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package gr.aegean.icsd.icarus.util.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
