d318f708cc63236e363ec62a3a529d8064e4f5e0
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-04 10:03:52 +0200

    Implemented repository for Test entity

--- src/main/java/gr/aegean/icsd/icarus/test/TestRepository.java
+++ src/main/java/gr/aegean/icsd/icarus/test/TestRepository.java
@@ -1,4 +1,8 @@
 package gr.aegean.icsd.icarus.test;
 
-public interface TestRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
 }
