d4a2e3e5781d4c3b46fc76142d9e7425b9aaac16
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-07 20:01:38 +0200

    Created Invalid Test Configuration Exception

---
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/InvalidTestConfigurationException.java
@@ -0,0 +1,10 @@
+package gr.aegean.icsd.icarus.util.exceptions;
+
+
+public class InvalidTestConfigurationException extends RuntimeException {
+
+    public InvalidTestConfigurationException(String message) {
+        super(message);
+    }
+
+}
