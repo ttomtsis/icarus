28905926d1fd73396f4a6e05d968c4626fe5a52e
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-11 13:42:48 +0200

    Created Test not Found exception

---
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/TestNotFoundException.java
@@ -0,0 +1,10 @@
+package gr.aegean.icsd.icarus.util.exceptions;
+
+public class TestNotFoundException extends RuntimeException {
+
+
+    public TestNotFoundException(String message) {
+        super(message);
+    }
+
+}
