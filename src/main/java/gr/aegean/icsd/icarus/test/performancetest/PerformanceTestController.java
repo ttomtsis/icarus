2364b022560c375f13ed0327c50436e5644ce96b
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-11 14:45:34 +0200

    Fully functional implementations of basic performance test use cases

--- src/main/java/gr/aegean/icsd/icarus/test/performancetest/PerformanceTestController.java
+++ src/main/java/gr/aegean/icsd/icarus/test/performancetest/PerformanceTestController.java
@@ -1,11 +1,9 @@
 package gr.aegean.icsd.icarus.test.performancetest;
 
 
+import gr.aegean.icsd.icarus.util.PatchDocument;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -27,6 +25,16 @@ public class PerformanceTestController {
 
 
 
+    @GetMapping("/{testId}")
+    public ResponseEntity<PerformanceTestModel> searchTest(@PathVariable Long testId) {
+
+        PerformanceTest requestedTest = service.searchTest(testId);
+
+        PerformanceTestModel requestedTestModel = assembler.toModel(requestedTest);
+
+        return ResponseEntity.ok().body(requestedTestModel);
+    }
+
     @PostMapping(consumes = "application/json")
     public ResponseEntity<PerformanceTestModel> createTest(@RequestBody PerformanceTestModel testModel) {
 
@@ -44,7 +52,29 @@ public class PerformanceTestController {
         return ResponseEntity.created(location).body(savedTestModel);
     }
 
+    @PutMapping(value = "/{testId}", consumes = "application/json")
+    public ResponseEntity<Void> updateTest(@PathVariable Long testId, @RequestBody PerformanceTestModel testModel) {
 
+        service.updateTest(testId, testModel);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping(value = "/{testId}", consumes = "application/json")
+    public ResponseEntity<Void> updateTestMetrics(@PathVariable Long testId, @RequestBody PatchDocument patchDoc) {
+
+        service.updateTestMetrics(testId, patchDoc);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{testId}")
+    public ResponseEntity<Void> deleteTest(@PathVariable Long testId) {
+
+        service.deleteTest(testId);
+
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
--- src/main/java/gr/aegean/icsd/icarus/test/performancetest/PerformanceTestService.java
+++ src/main/java/gr/aegean/icsd/icarus/test/performancetest/PerformanceTestService.java
@@ -1,10 +1,20 @@
 package gr.aegean.icsd.icarus.test.performancetest;
 
 import gr.aegean.icsd.icarus.test.TestRepository;
+import gr.aegean.icsd.icarus.util.PatchDocument;
+import gr.aegean.icsd.icarus.util.enums.Metric;
+import gr.aegean.icsd.icarus.util.enums.PatchOperation;
 import gr.aegean.icsd.icarus.util.exceptions.InvalidTestConfigurationException;
+import gr.aegean.icsd.icarus.util.exceptions.TestNotFoundException;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 
 @Service
@@ -20,6 +30,14 @@ public class PerformanceTestService {
     }
 
 
+
+    public PerformanceTest searchTest(@NotNull @Positive Long testId) {
+
+        return (PerformanceTest) repository.findById(testId)
+                .orElseThrow(() -> new TestNotFoundException
+                        ("Performance Test with id: " + testId + " was not found"));
+
+    }
 
     public PerformanceTest createTest(@NotNull PerformanceTest newTest) {
 
@@ -44,6 +62,85 @@ public class PerformanceTestService {
         return repository.save(newTest);
 
     }
+
+    public void updateTest(@NotNull @Positive Long testId, @NotNull PerformanceTestModel test) {
+
+        PerformanceTest requestedTest = (PerformanceTest) repository.findById(testId).orElseThrow( () ->
+                new TestNotFoundException
+                        ("Performance Test with ID: " + testId + " was not found"));
+
+        setIfNotBlank(requestedTest::setName, test.getName());
+        setIfNotBlank(value -> requestedTest.setHttpMethod(HttpMethod.valueOf(value)), test.getHttpMethod());
+        setIfNotBlank(requestedTest::setDescription, test.getDescription());
+        setIfNotBlank(requestedTest::setPathVariable, test.getPathVariable());
+        setIfNotBlank(requestedTest::setPath, test.getPath());
+        setIfNotBlank(requestedTest::setPathVariableValue, test.getPathVariableValue());
+        setIfNotBlank(requestedTest::setRequestBody, test.getRequestBody());
+
+        if (!test.getChosenMetrics().isEmpty()) {
+            requestedTest.setChosenMetrics(test.getChosenMetrics());
+        }
+
+        LoggerFactory.getLogger("bobz").warn("path: " + test.getPath());
+        repository.save(requestedTest);
+
+    }
+
+    private void setIfNotBlank(Consumer<String> setter, String value) {
+
+        if (StringUtils.isNotBlank(value)) {
+            setter.accept(value);
+        }
+    }
+
+    public void updateTestMetrics(@NotNull @Positive Long testId, @NotNull PatchDocument patchDoc) {
+
+        if (StringUtils.isBlank(patchDoc.getOp().toString()) ||
+                (StringUtils.isBlank(patchDoc.getValue()) && !patchDoc.getOp().equals(PatchOperation.REMOVE))
+        ) {
+
+            throw new HttpMessageNotReadableException("Patch document is missing required fields");
+
+        }
+
+        Metric patchedMetric;
+        try {
+            patchedMetric = Metric.valueOf(patchDoc.getValue());
+        }
+        catch (IllegalArgumentException ex) {
+            throw new HttpMessageNotReadableException(patchDoc.getValue() + " is not a valid Metric name", ex);
+        }
+
+
+        PerformanceTest requestedTest = (PerformanceTest) repository.findById(testId).orElseThrow( () ->
+                new TestNotFoundException
+                        ("Performance Test with ID: " + testId + " was not found"));
+
+
+        if (patchDoc.getOp().equals(PatchOperation.ADD)) {
+            requestedTest.addMetric(patchedMetric);
+        }
+
+        if (patchDoc.getOp().equals(PatchOperation.REMOVE)) {
+            requestedTest.removeMetric(patchedMetric);
+        }
+
+    }
+
+    public void deleteTest(@NotNull @Positive Long testId) {
+
+        if (repository.existsById(testId)) {
+
+            repository.deleteById(testId);
+
+        } else {
+
+            throw new TestNotFoundException("Performance Test with id " + testId + " does not exist");
+
+        }
+
+    }
+
 
 
 }
