e5c01609e0fa4d02e59c29a2626cdec2a72bc0aa
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-14 13:42:08 +0200

    Created exception for invalid test states

---
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/InvalidTestStateException.java
@@ -0,0 +1,14 @@
+package gr.aegean.icsd.icarus.util.exceptions;
+
+import gr.aegean.icsd.icarus.util.enums.TestState;
+
+public class InvalidTestStateException extends RuntimeException {
+
+    public InvalidTestStateException(Long testId, TestState invalidState, TestState expectedState) {
+
+        super("Test with id: " + testId + " is not in a valid state for this operation. \n" +
+                "Current State: " + invalidState.toString() + " , Expected State: " + expectedState.toString());
+    }
+
+
+}
