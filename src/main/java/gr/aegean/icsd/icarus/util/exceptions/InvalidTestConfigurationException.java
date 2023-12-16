c6a611ba4456a34a490e17749a7307d81fa2686b
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-16 21:41:02 +0200

    Minor improvements

--- src/main/java/gr/aegean/icsd/icarus/util/exceptions/InvalidTestConfigurationException.java
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/InvalidTestConfigurationException.java
@@ -3,8 +3,15 @@ package gr.aegean.icsd.icarus.util.exceptions;
 
 public class InvalidTestConfigurationException extends RuntimeException {
 
+
+    public InvalidTestConfigurationException(Long testId, String message) {
+        super("Test with ID: " + testId + " " + message);
+    }
+
+
     public InvalidTestConfigurationException(String message) {
         super(message);
     }
+
 
 }
