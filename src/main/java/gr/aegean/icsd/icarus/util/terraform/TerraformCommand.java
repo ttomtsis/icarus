b2f113de2372eacc6668dc54babc6c6c0f2d9504
Thomas Tomtsis <ttomtsis@protonmail.com>
2023-12-17 17:57:53 +0200

    Added supported terraform commands in an enum

---
+++ src/main/java/gr/aegean/icsd/icarus/util/terraform/TerraformCommand.java
@@ -0,0 +1,27 @@
+package gr.aegean.icsd.icarus.util.terraform;
+
+public enum TerraformCommand {
+
+
+    INIT("terraform", "init"),
+    APPLY("terraform", "apply", "-auto-approve"),
+    DESTROY("terraform", "destroy", "-auto-approve"),
+    OUTPUT("terraform", "output", "-raw");
+
+
+    private final String[] command;
+
+
+
+    TerraformCommand(String... command) {
+        this.command = command;
+    }
+
+
+    public String[] get() {
+        return command;
+    }
+
+
+}
+
