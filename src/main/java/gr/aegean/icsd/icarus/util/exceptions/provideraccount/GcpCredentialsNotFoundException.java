a4d294bef515a0c299c1a6648801aa28647de2a7
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-07 19:57:15 +0200

    Removed redundant and no longer used exceptions

--- src/main/java/gr/aegean/icsd/icarus/util/exceptions/provideraccount/GcpCredentialsNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package gr.aegean.icsd.icarus.util.exceptions.provideraccount;
-
-public class GcpCredentialsNotFoundException extends RuntimeException {
-
-    public GcpCredentialsNotFoundException(String message) {
-        super(message);
-    }
-
-}
