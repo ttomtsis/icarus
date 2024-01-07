6b7edacefdfcfc0471916baa4a03d5d5bdb8d583
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-07 20:06:24 +0200

    Implemented exception handlers for different kinds of exceptions

---
+++ src/main/java/gr/aegean/icsd/icarus/util/exceptions/controlleradvice/SqlExceptionHandler.java
@@ -0,0 +1,46 @@
+package gr.aegean.icsd.icarus.util.exceptions.controlleradvice;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+public class SqlExceptionHandler {
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+
+        String errorMessage = ex.getMostSpecificCause().getMessage();
+        String responseMessage = "An error occurred: ";
+
+        if (errorMessage.contains("duplicate key value violates unique constraint")) {
+            String field = errorMessage.split("\\(")[1].split("\\)")[0];
+            String value = errorMessage.split("=")[1].split(" ")[0];
+            responseMessage += "Duplicate field: " + field + "\n With value: " + value + ", already exists in the database.";
+        }
+
+        else if (errorMessage.contains("null value in column")) {
+            String field = errorMessage.split("null value in column \"")[1].split("\"")[0];
+            responseMessage += "Null value violates not-null constraint for field: " + field;
+
+        } else if (errorMessage.contains("not-null property references a null or transient value")) {
+            responseMessage += "A required field is null.";
+
+        } else if (errorMessage.contains("could not execute statement")) {
+            responseMessage += "There was a problem executing a statement.";
+
+        } else {
+            responseMessage += errorMessage;
+        }
+
+        return new ResponseEntity<>(responseMessage, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
