3efa24536b236cdb58e2f22001ceb998cc65d6c3
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-10 00:31:00 +0200

    Created a controller for Report entities

---
+++ src/main/java/gr/aegean/icsd/icarus/report/ReportController.java
@@ -0,0 +1,10 @@
+package gr.aegean.icsd.icarus.report;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v0/")
+public class ReportController {
+}
