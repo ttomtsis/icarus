5bd68f7b28b2032be5eda6391b21e030c62ebce2
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-10 19:28:55 +0200

    Report service can now create Reports for functional Tests

--- src/main/java/gr/aegean/icsd/icarus/report/ReportService.java
+++ src/main/java/gr/aegean/icsd/icarus/report/ReportService.java
@@ -1,48 +1,81 @@
 package gr.aegean.icsd.icarus.report;
 
 import gr.aegean.icsd.icarus.resourceconfiguration.ResourceConfiguration;
+import gr.aegean.icsd.icarus.test.Test;
+import gr.aegean.icsd.icarus.test.TestRepository;
 import gr.aegean.icsd.icarus.testexecution.TestExecution;
+import gr.aegean.icsd.icarus.testexecution.TestExecutionRepository;
+import gr.aegean.icsd.icarus.util.annotations.ValidFilePath.ValidFilePath;
 import gr.aegean.icsd.icarus.util.exceptions.async.TestExecutionFailedException;
+import gr.aegean.icsd.icarus.util.exceptions.entity.EntityNotFoundException;
+import gr.aegean.icsd.icarus.util.security.UserUtils;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.eclipse.birt.report.engine.api.*;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
+import java.io.ByteArrayOutputStream;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
-import static gr.aegean.icsd.icarus.IcarusConfiguration.*;
+import static gr.aegean.icsd.icarus.IcarusConfiguration.METRIC_RESULT_REPORT_TEMPLATE_LOCATION;
+import static gr.aegean.icsd.icarus.IcarusConfiguration.TEST_CASE_RESULT_REPORT_TEMPLATE_LOCATION;
 
 
 @Service
+@Transactional
+@Validated
 public class ReportService {
 
 
     private final IReportEngine birtReportEngine;
+    private final TestRepository testRepository;
+    private final TestExecutionRepository testExecutionRepository;
 
 
 
-    public ReportService(IReportEngine reportEngine) {
+    public ReportService(IReportEngine reportEngine, TestRepository testRepository,
+                         TestExecutionRepository testExecutionRepository) {
+
         this.birtReportEngine = reportEngine;
+        this.testRepository = testRepository;
+        this.testExecutionRepository = testExecutionRepository;
     }
 
 
 
-    public Report createReport() {
-        return new Report();
+    public Report getReport(Long testId, Long executionID) {
+
+        Test associatedTest = testRepository.findTestByIdAndCreator(testId, UserUtils.getLoggedInUser())
+                .orElseThrow( () -> new EntityNotFoundException(Test.class, testId));
+
+        TestExecution parentExecution = testExecutionRepository.findTestExecutionByIdAndParentTestAndCreator
+                        (executionID, associatedTest, UserUtils.getLoggedInUser())
+                .orElseThrow( () -> new EntityNotFoundException(TestExecution.class, executionID));
+
+        if (parentExecution.getReport() == null) {
+            throw new EntityNotFoundException
+                    ("Test execution: " + executionID + " does not have a Report associated with it");
+        }
+
+        return parentExecution.getReport();
     }
 
-    public Report createFunctionalTestReport(TestExecution execution, String username) {
 
-        String outputDirectory = OUTPUT_REPORT_DIRECTORY + "\\" + username + "\\" + "Reports";
-        String outputFilePath = outputDirectory + "\\" + execution.getParentTest().getName() + "-"
+    public Report createFunctionalTestReport(@NotNull TestExecution execution) {
+
+        String documentName = execution.getParentTest().getName() + "-"
                 + execution.getDeploymentId() + "-Report.pdf";
 
         Map<String, Object> parameters = new HashMap<>();
 
         parameters.put("ExecutionID", "" + execution.getId());
-        parameters.put("authorUsername", username);
+        parameters.put("authorUsername", execution.getCreator().getUsername());
 
         // TODO: This will likely be cause for future issues.
         //  The root of this issue is the implementation of Functional Test
@@ -61,25 +94,24 @@ public class ReportService {
         parameters.put("creationDate", formattedTimestamp);
 
 
-        generateReport(parameters, TEST_CASE_RESULT_REPORT_TEMPLATE_LOCATION, outputFilePath);
+        byte[] documentReport = generateReport(parameters, TEST_CASE_RESULT_REPORT_TEMPLATE_LOCATION);
 
-
-        return null;
+        return new Report(execution, documentReport, documentName);
     }
 
-    public Report createPerformanceTestReport(TestExecution testExecution, String username) {
 
-        String outputDirectory = OUTPUT_REPORT_DIRECTORY + "\\" + username + "\\" + "Reports";
+    public Report createPerformanceTestReport(@NotNull TestExecution testExecution) {
 
         Map<String, Object> parameters = new HashMap<>();
 
-        generateReport(parameters, METRIC_RESULT_REPORT_TEMPLATE_LOCATION, outputDirectory);
+        generateReport(parameters, METRIC_RESULT_REPORT_TEMPLATE_LOCATION);
 
-        return null;
+        return new Report();
     }
 
 
-    public void generateReport(Map<String, Object> parameters, String reportDesignFilePath, String outputFilePath) {
+    public byte[] generateReport(@NotNull Map<String, Object> parameters,
+                                 @NotBlank @ValidFilePath String reportDesignFilePath) {
 
         try {
             IReportRunnable design = birtReportEngine.openReportDesign(reportDesignFilePath);
@@ -88,12 +120,16 @@ public class ReportService {
             task.setParameterValues(parameters);
 
             PDFRenderOption options = new PDFRenderOption();
-            options.setOutputFileName(outputFilePath);
             options.setOutputFormat("pdf");
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            options.setOutputStream(byteArrayOutputStream);
 
             task.setRenderOption(options);
             task.run();
             task.close();
+
+            return byteArrayOutputStream.toByteArray();
         }
         catch (EngineException ex) {
             throw new TestExecutionFailedException(ex);
