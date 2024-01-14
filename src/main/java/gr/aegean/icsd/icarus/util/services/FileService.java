40ca5be357fa1c6ae1b69e7c5cdb0b55a3532b92
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-14 15:09:47 +0200

    Added delete file and save file functions

--- src/main/java/gr/aegean/icsd/icarus/util/services/FileService.java
+++ src/main/java/gr/aegean/icsd/icarus/util/services/FileService.java
@@ -1,21 +1,38 @@
 package gr.aegean.icsd.icarus.util.services;
 
+import gr.aegean.icsd.icarus.function.FunctionService;
 import gr.aegean.icsd.icarus.util.exceptions.async.TestExecutionFailedException;
 import jakarta.validation.constraints.NotBlank;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
 
 
 @Service
 @Validated
 public class FileService {
+
+
+    public void deleteFile(String fileDirectory) throws IOException {
+
+        Path functionSourceFilePath = Paths.get(fileDirectory);
+
+        if (!Files.exists(functionSourceFilePath)) {
+            LoggerFactory.getLogger(FunctionService.class).warn("Failed to delete file: {}\n" +
+                    " Specified file does not exist", fileDirectory);
+        }
+
+        else {
+            Files.delete(functionSourceFilePath);
+        }
+
+    }
 
 
     public void deleteDirectory(@NotBlank String dir) {
@@ -40,6 +57,31 @@ public class FileService {
         catch (IOException ex) {
             throw new TestExecutionFailedException(ex);
         }
+
+    }
+
+
+    public void saveFile(String fileDirectory, String fileName, MultipartFile file) throws IOException {
+
+        try {
+
+            Path dirPath = Paths.get(fileDirectory);
+            if (!Files.exists(dirPath)) {
+                Files.createDirectories(dirPath);
+            }
+
+            byte[] bytes = file.getBytes();
+            Path filePath = Paths.get(fileDirectory + "\\" + fileName);
+            Files.write(filePath, bytes);
+
+        } catch (IOException ex) {
+
+            String errorMessage = "Error when saving File to: " + fileDirectory + "\n" +
+                    Arrays.toString(ex.getStackTrace());
+
+            throw new IOException(errorMessage, ex);
+        }
+
     }
 
 
