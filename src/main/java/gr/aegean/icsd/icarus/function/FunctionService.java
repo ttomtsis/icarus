0b33114d2ae894f87ff6d6af5385fb762c483021
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-14 15:15:02 +0200

    Now using file service for function source operations

--- src/main/java/gr/aegean/icsd/icarus/function/FunctionService.java
+++ src/main/java/gr/aegean/icsd/icarus/function/FunctionService.java
@@ -3,20 +3,16 @@ package gr.aegean.icsd.icarus.function;
 import gr.aegean.icsd.icarus.icarususer.IcarusUser;
 import gr.aegean.icsd.icarus.util.exceptions.entity.EntityNotFoundException;
 import gr.aegean.icsd.icarus.util.security.UserUtils;
+import gr.aegean.icsd.icarus.util.services.FileService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.function.Consumer;
 
 import static gr.aegean.icsd.icarus.IcarusConfiguration.FUNCTION_SOURCES_DIRECTORY;
@@ -29,11 +25,13 @@ public class FunctionService {
 
 
     private final FunctionRepository functionRepository;
+    private final FileService fileService;
 
 
 
-    public FunctionService(FunctionRepository repository) {
+    public FunctionService(FunctionRepository repository, FileService fileService) {
         this.functionRepository = repository;
+        this.fileService = fileService;
     }
 
 
@@ -109,30 +107,10 @@ public class FunctionService {
         String functionSourceDirectory = FUNCTION_SOURCES_DIRECTORY + "\\" + UserUtils.getUsername() + "\\Functions";
         String functionSourceFileName = function.getName() + ".zip";
 
-        try {
+        fileService.saveFile(functionSourceDirectory, functionSourceFileName, functionSourceFile);
 
-            // Create function source directory if it does not exist
-            Path dirPath = Paths.get(functionSourceDirectory);
-            if (!Files.exists(dirPath)) {
-                Files.createDirectories(dirPath);
-            }
-
-            // Save function source to the directory
-            byte[] bytes = functionSourceFile.getBytes();
-            Path filePath = Paths.get(functionSourceDirectory + "\\" + functionSourceFileName);
-            Files.write(filePath, bytes);
-
-            // Set function source directory and function source filename
-            function.setFunctionSourceDirectory(functionSourceDirectory);
-            function.setFunctionSourceFileName(functionSourceFileName);
-
-        } catch (IOException ex) {
-
-            String errorMessage = "Error when saving function's source code to: " + functionSourceDirectory + "\n" +
-                    Arrays.toString(ex.getStackTrace());
-
-            throw new IOException(errorMessage, ex);
-        }
+        function.setFunctionSourceDirectory(functionSourceDirectory);
+        function.setFunctionSourceFileName(functionSourceFileName);
     }
 
     private void deleteFunctionSource(Function function)
@@ -141,22 +119,9 @@ public class FunctionService {
         String functionSourceDirectory = FUNCTION_SOURCES_DIRECTORY + "\\" + UserUtils.getUsername() + "\\Functions";
         String functionSourceFileName = function.getName() + ".zip";
 
-        Path functionSourceFilePath = Paths.get(functionSourceDirectory + "\\" + functionSourceFileName);
+        String functionSourceFilePath = functionSourceDirectory + "\\" + functionSourceFileName;
 
-        if (!Files.exists(Paths.get(functionSourceDirectory))) {
-            LoggerFactory.getLogger(FunctionService.class).warn("Source code directory" +
-                    " of Function {} does not exist", function.getName());
-        }
-
-        else if (!Files.exists(functionSourceFilePath)) {
-            LoggerFactory.getLogger(FunctionService.class).warn("Source code of Function {}" +
-                    " does not exist", function.getName());
-        }
-
-        else {
-            Files.delete(functionSourceFilePath);
-        }
-
+        fileService.deleteFile(functionSourceFilePath);
     }
 
 
