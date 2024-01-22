bac860a84990d51a9107d28fa600f25cdf9f94e7
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-22 15:16:37 +0200

    Configured Icarus to connect to Auth0's management API at startup and delete users that have been deleted by auth0

---
+++ src/main/java/gr/aegean/icsd/icarus/util/security/oauth2/Auth0DatabaseSynchronizer.java
@@ -0,0 +1,130 @@
+package gr.aegean.icsd.icarus.util.security.oauth2;
+
+import com.auth0.client.auth.AuthAPI;
+import com.auth0.client.mgmt.ManagementAPI;
+import com.auth0.client.mgmt.filter.UserFilter;
+import com.auth0.exception.Auth0Exception;
+import com.auth0.json.auth.TokenHolder;
+import com.auth0.json.mgmt.users.User;
+import com.auth0.json.mgmt.users.UsersPage;
+import com.auth0.net.Response;
+import gr.aegean.icsd.icarus.icarususer.IcarusUser;
+import gr.aegean.icsd.icarus.icarususer.IcarusUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static gr.aegean.icsd.icarus.util.security.oauth2.OAuth2Configuration.*;
+
+
+@Component
+public class Auth0DatabaseSynchronizer {
+
+
+    private static final Logger log = LoggerFactory.getLogger(Auth0DatabaseSynchronizer.class);
+
+
+    private final IcarusUserRepository icarusUserRepository;
+
+
+
+    public Auth0DatabaseSynchronizer(IcarusUserRepository icarusUserRepository) {
+        this.icarusUserRepository = icarusUserRepository;
+    }
+
+
+
+    @Bean
+    @Async
+    @Transactional
+    public void synchroniseDatabase() throws Auth0Exception {
+
+        ManagementAPI management = connectToManagementAPI();
+        Set<String> auth0Users = getAuth0Users(management);
+        synchroniseDatabase(auth0Users);
+    }
+
+
+    private ManagementAPI connectToManagementAPI() throws Auth0Exception {
+
+        log.warn("Connecting to Auth0 Management API");
+
+        // Auth0 domain shared across auth0 apps
+        String domain = DOMAIN;
+
+        // ID and Secret of the M2M Auth0 app
+        String id = CLIENT_ID;
+        String secret = CLIENT_SECRET;
+
+        // Identifier of the auth0 management api
+        String managementApiId = MANAGEMENT_API_ID;
+
+        AuthAPI auth = new AuthAPI(domain, id, secret);
+        Response<TokenHolder> response = auth.requestToken(managementApiId).execute();
+        TokenHolder holder = response.getBody();
+        String managementApiToken = holder.getAccessToken();
+
+        log.warn("Connection to Auth0 Management API successful");
+
+        return new ManagementAPI(domain, managementApiToken);
+    }
+
+
+    private Set<String> getAuth0Users(ManagementAPI management) throws Auth0Exception {
+
+        log.warn("Querying all registered users from Auth0");
+
+        int page = 0;
+        int perPage = 50;
+        boolean includeTotals = true;
+
+        Set<String> usernames = new HashSet<>();
+        UsersPage users;
+
+        do {
+            UserFilter userFilter = new UserFilter().withPage(page, perPage).withTotals(includeTotals);
+            users = management.users().list(userFilter).execute().getBody();
+
+            page++;
+
+            for (User user : users.getItems()) {
+                usernames.add(user.getUsername());
+            }
+
+        } while (users.getStart() + users.getLength() < users.getTotal());
+
+        log.warn("Querying Auth0 registered users successful");
+
+        return usernames;
+    }
+
+
+    private void synchroniseDatabase(Set<String> auth0Usernames) {
+
+        log.warn("Synchronizing local database with Auth0, deleting local users that no longer exist");
+
+        List<IcarusUser> localUsers = icarusUserRepository.findAll();
+
+        for (IcarusUser localUser : localUsers) {
+
+            String localUsersUsername = localUser.getUsername();
+
+            // Auth0 usernames are in lowercase
+            if (!auth0Usernames.contains(localUsersUsername.toLowerCase())) {
+                icarusUserRepository.deleteIcarusUserByUsername(localUsersUsername);
+                log.warn("Deleting local user: {}", localUsersUsername);
+            }
+        }
+
+        log.warn("Database synchronization complete");
+    }
+
+
+}
--- src/main/java/gr/aegean/icsd/icarus/util/security/oauth2/OAuth2Configuration.java
+++ src/main/java/gr/aegean/icsd/icarus/util/security/oauth2/OAuth2Configuration.java
@@ -9,11 +9,35 @@ public class OAuth2Configuration {
 
 
     public static String AUDIENCE;
+    public static String CLIENT_ID;
+    public static String CLIENT_SECRET;
+    public static String DOMAIN;
+    public static String MANAGEMENT_API_ID;
 
 
     @Value("${spring.security.oauth2.resourceserver.jwt.audiences}")
-    public void setAUDIENCE(String audience) {
+    public void setAudience(String audience) {
         OAuth2Configuration.AUDIENCE = audience;
+    }
+
+    @Value("${security.auth0.clientId}")
+    public void setClientId(String clientID) {
+        OAuth2Configuration.CLIENT_ID = clientID;
+    }
+
+    @Value("${security.auth0.clientSecret}")
+    public void setClientSecret(String clientSecret) {
+        OAuth2Configuration.CLIENT_SECRET = clientSecret;
+    }
+
+    @Value("${security.auth0.domain}")
+    public void setDomain(String domain) {
+        OAuth2Configuration.DOMAIN = domain;
+    }
+
+    @Value("${security.auth0.managementApiId}")
+    public void setManagementApiId(String managementApiId) {
+        OAuth2Configuration.MANAGEMENT_API_ID = managementApiId;
     }
 
 
