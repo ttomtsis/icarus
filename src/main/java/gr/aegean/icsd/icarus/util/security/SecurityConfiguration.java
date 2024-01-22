25c2c39724b72303b6aaf032084025770e3aed13
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-22 21:53:15 +0200

    Refactored code and moved relevant beans into http basic configuration

--- src/main/java/gr/aegean/icsd/icarus/util/security/SecurityConfiguration.java
+++ src/main/java/gr/aegean/icsd/icarus/util/security/SecurityConfiguration.java
@@ -1,8 +1,5 @@
 package gr.aegean.icsd.icarus.util.security;
 
-import gr.aegean.icsd.icarus.icarususer.IcarusUser;
-import gr.aegean.icsd.icarus.util.exceptions.IcarusConfigurationException;
-import gr.aegean.icsd.icarus.util.security.httpbasic.SqlAuthenticationManager;
 import gr.aegean.icsd.icarus.util.security.oauth2.Auth0AuthenticationConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
@@ -38,38 +31,8 @@ import static jakarta.servlet.DispatcherType.*;
 public class SecurityConfiguration {
 
 
-    public static int CREDENTIALS_EXPIRATION_PERIOD;
-    public static final int MINIMUM_PASSWORD_LENGTH = 8;
-    public static final int MAXIMUM_PASSWORD_LENGTH = 150;
-
-
-    @Value("${security.httpBasic.users.enableTestUser}")
-    private boolean enableTestAccounts;
-
-    @Value("${security.httpBasic.users.testUserUsername}")
-    private String testAccountUsername;
-
-    @Value("${security.httpBasic.users.testUserPassword}")
-    private String testAccountPassword;
-
-    @Value("${security.httpBasic.users.testUserEmail}")
-    private String testAccountEmail;
-
     @Value("${security.httpBasic.enableHttpBasic}")
     private boolean enableHttpBasic;
-
-
-    @Value("${security.httpBasic.credentialsExpirationPeriod}")
-    public void setCredentialsExpirationPeriod(Integer credentialsExpirationPeriod) {
-
-        if (credentialsExpirationPeriod != null && credentialsExpirationPeriod > 0) {
-            SecurityConfiguration.CREDENTIALS_EXPIRATION_PERIOD = credentialsExpirationPeriod;
-        }
-        else {
-            throw new IcarusConfigurationException("Credentials expiration period cannot be: "
-                    + credentialsExpirationPeriod);
-        }
-    }
 
 
 
@@ -146,41 +109,8 @@ public class SecurityConfiguration {
 
 
     @Bean
-    DelegatingPasswordEncoder createEncoder() {
-
-        DelegatingPasswordEncoder passwordEncoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
-
-        return passwordEncoder;
-    }
-
-
-    @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
-    }
-
-
-    /**
-     * If test accounts are enabled, this method creates them
-     * ( provided that they do not already exist in the database )
-     *
-     * @param userManager User manager that will be used to check for the account's existence
-     *
-     * @return true if the accounts were created
-     */
-    @Bean
-    boolean createTestUsers(SqlAuthenticationManager userManager) {
-
-        if (enableTestAccounts && enableHttpBasic && !userManager.userExists(testAccountUsername)) {
-
-            UserDetails testJournalist = new IcarusUser(testAccountUsername, testAccountPassword, testAccountEmail);
-            userManager.createUser(testJournalist);
-
-            return true;
-        }
-
-        return false;
     }
 
 
--- src/main/java/gr/aegean/icsd/icarus/util/security/httpbasic/HttpBasicConfiguration.java
+++ src/main/java/gr/aegean/icsd/icarus/util/security/httpbasic/HttpBasicConfiguration.java
@@ -1,15 +1,16 @@
 package gr.aegean.icsd.icarus.util.security.httpbasic;
 
+import gr.aegean.icsd.icarus.icarususer.IcarusUser;
 import gr.aegean.icsd.icarus.icarususer.IcarusUserRepository;
+import gr.aegean.icsd.icarus.util.exceptions.IcarusConfigurationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 
 /**
@@ -17,6 +18,40 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 public class HttpBasicConfiguration {
+
+
+    public static int CREDENTIALS_EXPIRATION_PERIOD;
+    public static final int MINIMUM_PASSWORD_LENGTH = 8;
+    public static final int MAXIMUM_PASSWORD_LENGTH = 150;
+
+
+    @Value("${security.httpBasic.users.enableTestUser}")
+    private boolean enableTestAccounts;
+
+    @Value("${security.httpBasic.users.testUserUsername}")
+    private String testAccountUsername;
+
+    @Value("${security.httpBasic.users.testUserPassword}")
+    private String testAccountPassword;
+
+    @Value("${security.httpBasic.users.testUserEmail}")
+    private String testAccountEmail;
+
+    @Value("${security.httpBasic.enableHttpBasic}")
+    private boolean enableHttpBasic;
+
+
+    @Value("${security.httpBasic.credentialsExpirationPeriod}")
+    public void setCredentialsExpirationPeriod(Integer credentialsExpirationPeriod) {
+
+        if (credentialsExpirationPeriod != null && credentialsExpirationPeriod > 0) {
+            HttpBasicConfiguration.CREDENTIALS_EXPIRATION_PERIOD = credentialsExpirationPeriod;
+        }
+        else {
+            throw new IcarusConfigurationException("Credentials expiration period cannot be: "
+                    + credentialsExpirationPeriod);
+        }
+    }
 
 
     private final IcarusUserRepository userRepository;
@@ -49,6 +84,39 @@ public class HttpBasicConfiguration {
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return authenticationProvider;
+    }
+
+
+    @Bean
+    DelegatingPasswordEncoder createEncoder() {
+
+        DelegatingPasswordEncoder passwordEncoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+
+        return passwordEncoder;
+    }
+
+
+    /**
+     * If test accounts are enabled, this method creates them
+     * ( provided that they do not already exist in the database )
+     *
+     * @param userManager User manager that will be used to check for the account's existence
+     *
+     * @return true if the accounts were created
+     */
+    @Bean
+    boolean createTestUsers(SqlAuthenticationManager userManager) {
+
+        if (enableTestAccounts && enableHttpBasic && !userManager.userExists(testAccountUsername)) {
+
+            UserDetails testJournalist = new IcarusUser(testAccountUsername, testAccountPassword, testAccountEmail);
+            userManager.createUser(testJournalist);
+
+            return true;
+        }
+
+        return false;
     }
 
 
