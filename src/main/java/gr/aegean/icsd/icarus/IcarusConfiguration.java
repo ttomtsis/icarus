061e60934bb98d3ca1fe9c1cd427596f8d8c4a27
Thomas Tomtsis <ttomtsis@protonmail.com>
2024-01-30 22:27:48 +0200

    Implemented check Icarus Configuration method

--- src/main/java/gr/aegean/icsd/icarus/IcarusConfiguration.java
+++ src/main/java/gr/aegean/icsd/icarus/IcarusConfiguration.java
@@ -1,29 +1,43 @@
 package gr.aegean.icsd.icarus;
 
 import gr.aegean.icsd.icarus.util.exceptions.IcarusConfigurationException;
+import gr.aegean.icsd.icarus.util.services.ProcessService;
+import org.apache.jmeter.util.JMeterUtils;
 import org.eclipse.birt.core.framework.Platform;
 import org.eclipse.birt.report.engine.api.EngineConfig;
 import org.eclipse.birt.report.engine.api.IReportEngine;
 import org.eclipse.birt.report.engine.api.IReportEngineFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
 import java.util.logging.Level;
+
+import static gr.aegean.icsd.icarus.util.jmeter.JMeterConfiguration.JMETER_HOME_DIRECTORY;
+import static gr.aegean.icsd.icarus.util.jmeter.JMeterConfiguration.JMETER_PROPERTIES_FILE;
 
 
 @Configuration
 public class IcarusConfiguration {
 
 
-    public static String FUNCTION_SOURCES_DIRECTORY;
+    private static final Logger log = LoggerFactory.getLogger(IcarusConfiguration.class);
 
+    private final ProcessService processService;
+
+
+    public static String FUNCTION_SOURCES_DIRECTORY;
     public static String TEST_CASE_RESULT_REPORT_TEMPLATE_LOCATION;
     public static String METRIC_RESULT_REPORT_TEMPLATE_LOCATION;
 
 
 
-    public IcarusConfiguration(@Value("${icarus.reports.metricResultReportTemplateLocation}")
+    public IcarusConfiguration(ProcessService processService,
+
+                               @Value("${icarus.reports.metricResultReportTemplateLocation}")
                                String metricResultReportTemplateLocation,
 
                                @Value("${icarus.reports.testCaseResultReportTemplateLocation}")
@@ -31,6 +45,8 @@ public class IcarusConfiguration {
 
                                @Value("${icarus.functionSourcesDirectory}")
                                String functionSourcesDirectory) {
+
+        this.processService = processService;
 
         IcarusConfiguration.FUNCTION_SOURCES_DIRECTORY = functionSourcesDirectory;
         IcarusConfiguration.TEST_CASE_RESULT_REPORT_TEMPLATE_LOCATION = testCaseResultReportTemplateLocation;
@@ -50,7 +66,7 @@ public class IcarusConfiguration {
             Platform.startup(config);
 
         } catch (Exception e) {
-
+            log.error("Unable to start the BiRT Engine: {}", e.getMessage());
             throw new IcarusConfigurationException("Could not start the Birt platform.", e);
         }
 
@@ -63,8 +79,31 @@ public class IcarusConfiguration {
 
 
     @Bean
-    public void checkIcarusConfiguration() {
-        //TODO: Check if terraform and jmeter can run, check configuration properties values
+    public boolean checkIcarusConfiguration(){
+
+        log.info("Checking terraform version");
+        try {
+            String terraformVersion = processService.runCommand("terraform", "-v");
+            log.info("Terraform version:\n{}", terraformVersion);
+        }
+        catch (IOException | InterruptedException ex) {
+            log.error("Fatal error: Icarus cannot find Terraform: {}", ex.getMessage());
+            throw new IcarusConfigurationException("Terraform not found");
+        }
+
+        log.info("Validating JMeter's configuration directory");
+        try {
+            JMeterUtils.setJMeterHome(JMETER_HOME_DIRECTORY);
+            JMeterUtils.loadJMeterProperties(JMETER_PROPERTIES_FILE);
+            JMeterUtils.initLocale();
+
+            log.info("JMeter's configuration directory is valid and located at: {}", JMETER_PROPERTIES_FILE);
+        } catch (Exception e) {
+            log.error("Error initializing JMeter: {}", e.getMessage());
+            throw new IcarusConfigurationException("Unable to initialize JMeter");
+        }
+
+        return true;
     }
 
 
--- src/main/java/gr/aegean/icsd/icarus/util/services/ProcessService.java
+++ src/main/java/gr/aegean/icsd/icarus/util/services/ProcessService.java
@@ -8,8 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 
@@ -50,6 +52,31 @@ public class ProcessService {
             throw new AsyncExecutionFailedException(ex);
         }
 
+    }
+
+    public String runCommand(@NotNull String ... command) throws InterruptedException, IOException {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(command);
+
+        log.info("Executing command: {}", Arrays.toString(command));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder output = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line + "\n");
+        }
+
+        int exitCode = process.waitFor();
+
+        if (exitCode != 0) {
+            log.error("Command: {} was not completed successfully", Arrays.toString(command));
+        }
+
+        return output.toString();
     }
 
 
